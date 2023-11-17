8898f4b969f1adfa008e6181ba04e6466e283acc
Mukesh2312 <49585682+Mukesh2312@users.noreply.github.com>
2023-11-17 20:06:48 +0530

    code

--- App.js
+++ App.js
@@ -1,1 +1,10 @@
+console.log("js connected")
 
+async function initiall() {
+    const response = await fetch('https://isro.vercel.app/api/centres');
+    const data = await response.json();
+
+    console.log(data);
+}
+
+initiall();
