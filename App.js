d01f596f76eb9807fcb652d7dc486312e1ba22d8
Mukesh2312 <49585682+Mukesh2312@users.noreply.github.com>
2023-11-18 12:33:54 +0530

    code

--- App.js
+++ App.js
@@ -1,4 +1,20 @@
-console.log("js connected")
+const cityBtn = document.getElementById('cityBtn');
+const stateBtn = document.getElementById('stateBtn');
+const centerBtn = document.getElementById('centerBtn')
+const headName = document.getElementById('headName');
+let txtColor = "#004AAD"
+
+// headName.style.color = txtColor;
+// function colorFirstLetter() {
+//     let letters = headName.innerText.split(" ");
+//     letters.forEach(element => {
+//         let word = element.charAt(0);
+
+//     });
+
+// }
+
+// colorFirstLetter();
 
 async function initiall() {
     const response = await fetch('https://isro.vercel.app/api/centres');
@@ -8,3 +24,5 @@ async function initiall() {
 }
 
 initiall();
+
+
